4323ee13286c13befd913b36f8aa16bb407735ad
0xSimeon <simeon.udoh45@gmail.com>
2023-05-18 00:31:02 +0100

    :lipstick: add natspec docs for the contract

--- packages/hardhat/contracts/MedicalRecord.sol
+++ packages/hardhat/contracts/MedicalRecord.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier:GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/// @title A Medical Record management and storage
+/// @author Simeon Udoh
+/// @dev All methods are live, use responsibly. 
+/// @custom:experimental This is an experimental contract still in active development.
+
 contract MedicalRecord {
 
+    /// @dev: This is to keep track of the patient count in the system. 
     using Counters for Counters.Counter;
     Counters.Counter public patientId;
 
+    /// @dev This is the structure for patient record. 
     struct PatientData {
         uint256 id;
         address patientWalletAddress;
@@ -17,6 +23,7 @@ contract MedicalRecord {
         uint256 dateCreated;
     }
     
+    /// @custom:event On-chain events to keep track of patientData
     event LogPatientData(
         uint256 id,
         address patientWalletAddress,
@@ -26,6 +33,8 @@ contract MedicalRecord {
     );
 
 
+    /// @dev stucture for patient reports
+    ///@notice feature still in active development. 
     struct PatientMedicalReport {
         address patientWalletAddress;
         string testResult;
@@ -33,26 +42,43 @@ contract MedicalRecord {
         uint256 dateCreated;
     }
 
-
+    ///@custom:event to keep track of patient reports
     event LogPatientMedicalReport(
         address patientWalletAddress,
         string testResult,
         bytes32 txHash,
         uint256 dateCreated
     );
-   
+
+    /***
+    @dev: State variables. 
+    @author: Simeon Udoh
+    */ 
+
     uint256 public patientCount;
 
     PatientData [] public patients;
     PatientMedicalReport [] public patientReports;
 
-    address public owner;
+    /***
+    @dev: This is the state variable for the contract owner.
+    @notice: See the constructor to see where this value is gotten. 
+     */
+    address public owner; 
+
+    /// @dev keep track of addresses in the system. 
+    ///@notice Mapping of address to boolean. 
     mapping(address => bool) public isExist;
 
+    /***
+    @dev: owner is defined automatically when the contract is deployed. 
+    @notice: deployer of contract === owner. 
+    */
     constructor(){
        owner = msg.sender;
     }
 
+    ///@dev 
     modifier onlyOwner {
         require(msg.sender == owner, "You are not the owner");
         _;
@@ -63,14 +89,20 @@ contract MedicalRecord {
         _; 
     }
 
+ 
+
+// Param: Patient wallet address, IPFS hash
      function addPatient( 
         address _patientWalletAddress,
         string memory _patientIPFSHash
         
-        ) public onlyOwner{
+        ) public onlyOwner  {
         require(isExist[_patientWalletAddress] != true, "Patient record already exist");
         require(bytes(_patientIPFSHash).length > 0, "IPFS Hash required");
         uint256 _id = patientId.current();
+
+    
+        // bytes32 getHash =  keccak256(abi.encode(_patientWalletAddress, _patientIPFSHash));
 
         string[] memory arr;
         bytes32 txHash = blockhash(block.number);
